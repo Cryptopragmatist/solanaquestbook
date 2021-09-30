7bc79182b6103021ef06799582371ad00a11f08d
Noderunner.eth <tristan.alberio@gmail.com>
2021-09-30 12:24:48 +0800

    creating acc on solana and transaction

--- index.js
+++ index.js
@@ -36,27 +36,28 @@ async function createAccount() {
     "campaign1",
     new PublicKey("<ENTER PROGRAM ID HERE>"), //program id ,PDA. the new acc is associated with this program and will control the campaign acc
   );
-  const lamports = await connection.getMinimumBalanceForRentExemption(
+  const lamports = await connection.getMinimumBalanceForRentExemption( // get the minimum balance for rent exemption
     1024,
   );
-  const instruction = SystemProgram.createAccountWithSeed({
+  const instruction = SystemProgram.createAccountWithSeed({ // tell solana to create a new acc in the blockchain
     fromPubkey: signer.publicKey,
     basePubkey: signer.publicKey,
     seed: "campaign1",
-    newAccountPubkey,
+    newAccountPubkey,// create an account via System program using this public key that is connected to the program using program ID
     lamports,
-    space: 1024,
+    space: 1024, //need to allocate space to store the data
     programId : new PublicKey("<ENTER PROGRAM ID HERE>"),
   });
-  const transaction = new Transaction().add(
+  const transaction = new Transaction().add( //execute instruction on the blockchain (transaction)
     instruction
   );
 
   console.log(`The address of campaign1 account is : ${newAccountPubkey.toBase58()}`);
 
-  await sendAndConfirmTransaction(connection, transaction, [signer]);
+  await sendAndConfirmTransaction(connection, transaction, [signer]); //signed transaction
 
 }
 
 establishConnection(); //call the function 
 createAccount();
+//the rent is exempt if you store a minimum amount of lamports in this account.If you don’t pay the rent and you dont have the minimum balance, the account gets deleted after some time
